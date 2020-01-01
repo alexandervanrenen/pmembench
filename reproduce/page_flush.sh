eab9999d873870cdcaf1bb24137bfcdb99bddab4
alex <renen@in.tum.de>
2020-01-01 10:16:55 +0100

    changed file name in script

--- reproduce/page_flush.sh
+++ reproduce/page_flush.sh
@@ -6,11 +6,11 @@ PAGE_COUNT=100000 # ~1.6GB
 REPETITIONS=10
 
 # Experiment 1: 1 thread, _x_ dirty cls, streaming
-echo "" > results/page_flush_cls_t1.txt
+echo "" > results/page_flush_cls_1t.txt
 clang++ page_flush/page_flush.cpp -std=c++17 -g0 -O3  -march=native -DNDEBUG -DSTREAMING=1 -Invml/src/include/ nvml/src/nondebug/libpmem.a nvml/src/nondebug/libpmemblk.a -lpthread -lndctl -ldaxctl
 THREAD_COUNT=1
 for DIRTY_CL_COUNT in `seq 4 4 256`; do
-  ./a.out ${PAGE_COUNT} ${DIRTY_CL_COUNT} ${THREAD_COUNT} ${REPETITIONS} ${PMEM_PATH}/file | tee -a results/page_flush_cls_t1.txt
+  ./a.out ${PAGE_COUNT} ${DIRTY_CL_COUNT} ${THREAD_COUNT} ${REPETITIONS} ${PMEM_PATH}/file | tee -a results/page_flush_cls_1t.txt
 done
 
 # Experiment 2: _x_ thread, 16 dirty cls, streaming
@@ -22,9 +22,9 @@ for THREAD_COUNT in `seq 1 30`; do
 done
 
 # Experiment 3: 7 thread, _x_ dirty cls, streaming
-echo "" > results/page_flush_cls_t7.txt
+echo "" > results/page_flush_cls_7t.txt
 clang++ page_flush/page_flush.cpp -std=c++17 -g0 -O3  -march=native -DNDEBUG -DSTREAMING=1 -Invml/src/include/ nvml/src/nondebug/libpmem.a nvml/src/nondebug/libpmemblk.a -lpthread -lndctl -ldaxctl
 THREAD_COUNT=7
 for DIRTY_CL_COUNT in `seq 4 4 256`; do
-  ./a.out ${PAGE_COUNT} ${DIRTY_CL_COUNT} ${THREAD_COUNT} ${REPETITIONS} ${PMEM_PATH}/file | tee -a results/page_flush_cls_t7.txt
+  ./a.out ${PAGE_COUNT} ${DIRTY_CL_COUNT} ${THREAD_COUNT} ${REPETITIONS} ${PMEM_PATH}/file | tee -a results/page_flush_cls_7t.txt
 done
