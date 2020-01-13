c5d2c60b7d0b2a327882233a6daabdb5b861b0ed
alex <renen@in.tum.de>
2020-01-13 13:23:27 +0100

    disabled unused logging techniques

--- logging/logging.cpp
+++ logging/logging.cpp
@@ -161,21 +161,21 @@ int main(int argc, char **argv)
       }
 
       // Classic cached
-      {
-         LogWriterClassicCached wal(nvm);
-         vector<LogWriterClassicCached::Entry *> entries = LogWriterClassicCached::CreateRandomEntries(memory, entry_size / 8, entry_size / 8, LOG_PAYLOAD_SIZE, ranny);
-         ub8 ns_spend = RunWithTiming([&]() {
-            for (LogWriterClassicCached::Entry *entry : entries) {
-               wal.AddLogEntry(*entry);
-            }
-         });
-         if (TABLE_VIEW) {
-            printf("%20f", ns_spend * 1.0 / entries.size());
-            fflush(stdout);
-         } else {
-            PrintResult("classicCached", entry_size, ns_spend * 1.0 / entries.size(), wal.GetWrittenByteCount());
-         }
-      }
+      //      {
+      //         LogWriterClassicCached wal(nvm);
+      //         vector<LogWriterClassicCached::Entry *> entries = LogWriterClassicCached::CreateRandomEntries(memory, entry_size / 8, entry_size / 8, LOG_PAYLOAD_SIZE, ranny);
+      //         ub8 ns_spend = RunWithTiming([&]() {
+      //            for (LogWriterClassicCached::Entry *entry : entries) {
+      //               wal.AddLogEntry(*entry);
+      //            }
+      //         });
+      //         if (TABLE_VIEW) {
+      //            printf("%20f", ns_spend * 1.0 / entries.size());
+      //            fflush(stdout);
+      //         } else {
+      //            PrintResult("classicCached", entry_size, ns_spend * 1.0 / entries.size(), wal.GetWrittenByteCount());
+      //         }
+      //      }
 
       // Classic aligned
       {
@@ -229,21 +229,21 @@ int main(int argc, char **argv)
       }
 
       // Header dancing
-      {
-         LogWriterHeaderDancing wal(nvm);
-         vector<LogWriterHeaderDancing::Entry *> entries = LogWriterHeaderDancing::CreateRandomEntries(memory, entry_size / 8, entry_size / 8, LOG_PAYLOAD_SIZE, ranny);
-         ub8 ns_spend = RunWithTiming([&]() {
-            for (LogWriterHeaderDancing::Entry *entry : entries) {
-               wal.AddLogEntry(*entry);
-            }
-         });
-         if (TABLE_VIEW) {
-            printf("%20f", ns_spend * 1.0 / entries.size());
-            fflush(stdout);
-         } else {
-            PrintResult("headerDanc", entry_size, ns_spend * 1.0 / entries.size(), wal.GetWrittenByteCount());
-         }
-      }
+      //      {
+      //         LogWriterHeaderDancing wal(nvm);
+      //         vector<LogWriterHeaderDancing::Entry *> entries = LogWriterHeaderDancing::CreateRandomEntries(memory, entry_size / 8, entry_size / 8, LOG_PAYLOAD_SIZE, ranny);
+      //         ub8 ns_spend = RunWithTiming([&]() {
+      //            for (LogWriterHeaderDancing::Entry *entry : entries) {
+      //               wal.AddLogEntry(*entry);
+      //            }
+      //         });
+      //         if (TABLE_VIEW) {
+      //            printf("%20f", ns_spend * 1.0 / entries.size());
+      //            fflush(stdout);
+      //         } else {
+      //            PrintResult("headerDanc", entry_size, ns_spend * 1.0 / entries.size(), wal.GetWrittenByteCount());
+      //         }
+      //      }
 
       // Header aligned dancing
       {
